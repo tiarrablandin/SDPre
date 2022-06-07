0d274820e97fa5c4d3f7935be609cb2353769854
tblandin <tiarra.blandin@outlook.com>
2022-06-06 23:32:48 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Tiarra Blandin";
+    int age = 31;
+    String state = "Florida";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
