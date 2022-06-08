6ca63ade004e7834c4b31f1d7314d147f217e2c8
tblandin <tiarra.blandin@outlook.com>
2022-06-07 20:46:19 -0600

    complete Objective3Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,12 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int num1  = 1 + (int)(Math.random() *6);
+    int num2 = 1 + (int)(Math.random() *6);
+    int num3 = 1 + (int)(Math.random() *6);
+    int sum = num1 + num2 + num3;
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num3);
+    System.out.println(sum);
+  }
+}
