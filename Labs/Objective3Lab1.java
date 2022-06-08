f7d77f1f329d856b58632a4ddd29848aa70cc86a
tblandin <tiarra.blandin@outlook.com>
2022-06-07 19:09:35 -0600

    completed Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+  public static void main (String[] args) {
+    int num1 = 15;
+    int num2 = 6;
+    int num3 = 2;
+    double average = 7.66666666667;
+    System.out.println(average);
+  }
+}
