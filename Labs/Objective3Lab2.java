09d64ccb1c81f12e4cd19bf96de0231fdeecd04e
tblandin <tiarra.blandin@outlook.com>
2022-06-07 20:33:31 -0600

    complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 3.16227766017;
+    double side2 = 2.82842712475;
+    double side3 = side1 * 3.16227766017;
+    double side4 = side2 * 2.82842712475;
+    double hypotenuse = side3 + side4;
+    System.out.println(side3);
+    System.out.println(side4);
+    System.out.println(hypotenuse);
+  }
+}
