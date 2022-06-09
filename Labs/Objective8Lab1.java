4f321a0113bcc4a53ce258623e0f63fbd2131dd6
tblandin <tiarra.blandin@outlook.com>
2022-06-09 15:33:34 -0600

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int num1 = 1; num1 <= 10; num1++) {
+      System.out.println(num1);
+    }
+    for (int num2 = 10; num2 > 0; num2--) {
+      System.out.println(num2);
+    }
+  }
+}
