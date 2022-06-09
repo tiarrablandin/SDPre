3e0b412f793a037241d4624bda8bbeb469715074
tblandin <tiarra.blandin@outlook.com>
2022-06-09 00:31:25 -0600

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int totalSum = 0;
+    int increments = 0;
+
+    while (increments < 20) {
+      increments++;
+      totalSum += increments;
+      System.out.println(totalSum);
+
+    }
+  }
+}
