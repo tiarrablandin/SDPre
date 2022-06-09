2b8307ce14490821f170002ef135e150b0ff5cda
tblandin <tiarra.blandin@outlook.com>
2022-06-08 21:21:25 -0600

    complete Objective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,17 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+
+  int num1 = 5;
+  int num2 = 15;
+
+  boolean bool1 = (num1 < num2);
+
+  if (num1 > num2) {
+    System.out.println("5 is greater than 15");
+  }
+
+  else if (num1 < num2) {
+    System.out.println("15 is greater than 5");
+    }
+  }
+}
